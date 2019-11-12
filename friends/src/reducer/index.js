25dba245d70089398767f29b7896bec2a54b943e
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-12 14:25:38 -0800

    Add action types to reducer.

--- friends/src/reducer/index.js
+++ friends/src/reducer/index.js
@@ -1,4 +1,4 @@
-import {LOADING , SUCCESS , FAILURE}
+import {LOADING , SUCCESS , FAILURE} from '../actions/index.js';
 
 const initialState = [{
     name: '',
@@ -7,9 +7,30 @@ const initialState = [{
 }]
 
 export const reducer = (state = initialState, action) => {
-// console.log("reducer", action);
-switch(action.type){
-    default: 
-    return state;
-}
+    // console.log("reducer", action);
+    switch (action.type) {
+        case LOADING:
+            return {
+                ...state,
+                error: null,
+                isLoading: true
+            }
+        case SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload,
+                isLoading: false
+            }
+        case FAILURE:
+            return {
+                ...state,
+                smurfs: [],
+                error: action.payload,
+                isLoading: false
+            }
+        // case DELETE_SMURF_SUCCESS:
+        //     const newState = ([...state, state])
+        default:
+            return state;
+    }
 }
