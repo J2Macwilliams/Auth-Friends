3838628bdc6e6b83e1d79bfe10efee980cbbd6fb
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-12 13:28:38 -0800

    Clean up code.

---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export class LoginForm extends Component {
+    state ={
+        
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default LoginForm
